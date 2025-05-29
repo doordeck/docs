63e38e6cce07ee02a824241209402ce407e6022c
Bernat <bernatgarciacarbo@gmail.com>
2025-05-29 19:36:42 +0200

    config test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,8 @@ const config: Config = {
   url: 'https://developer.doordeck.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
