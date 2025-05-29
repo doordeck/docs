cbea7f085f81a0747eea0463ae602d0479c82228
Bernat Carbó <41543253+BernatCarbo@users.noreply.github.com>
2025-05-29 12:50:38 +0200

    Update docusaurus.config.ts
    Co-authored-by: Michael Barnwell <michael@doordeck.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Doordeck, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Doordeck Limited. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
