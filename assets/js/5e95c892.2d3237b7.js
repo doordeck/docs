7a2f424a8c923631382973c7beaadae0eee1f09b
BernatCarbo <BernatCarbo@users.noreply.github.com>
2026-01-30 23:21:04 +0000

    deploy: 1bfbf62b9806de0ee0e52002ab257ea77a4afebe

---
+++ assets/js/5e95c892.2d3237b7.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdoordeck_docs=globalThis.webpackChunkdoordeck_docs||[]).push([[9647],{7121(e,s,r){r.r(s),r.d(s,{default:()=>h});r(6540);var c=r(4164),o=r(7559),d=r(5500),a=r(2831),u=r(4060),l=r(4848);function h(e){return(0,l.jsx)(d.e3,{className:(0,c.A)(o.G.wrapper.docsPages),children:(0,l.jsx)(u.A,{children:(0,a.v)(e.route.routes)})})}}}]);
